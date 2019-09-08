4d15805a82b24946efb5b270fcef09df59b01977
rambod <gh.rambod@gmail.com>
2019-09-08 16:13:16 +0430

    fix Setup.py file

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
 setup(
   name = 'MailToolsBox',
   packages = ['MailToolsBox'],
   version = '0.0.2',
   license='MIT',
   description = 'Mail Tools boxes to make developer life easier on build mail sender or even mail server',   # Give a short description about your library
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'Rambod Ghashghai',                   # Type in your name
   author_email = 'rambod@ramai.io',      # Type in your E-Mail
   url = 'https://www.ramai.io',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/rambod/MailToolsBox/archive/0.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/rambod/MailToolsBox/archive/0.0.3.tar.gz',    # I explain this later on
   keywords = ['Mail', 'Server', 'smtp' ,  'send' , 'email', 'tools', 'box'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
+    'Topic :: Communications :: Email',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.6',
