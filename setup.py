6e35c84c958850777e2303f0da1d88fcd766dd74
rambod <gh.rambod@gmail.com>
2019-09-08 16:54:56 +0430

    fix Setup.py file version

--- setup.py
+++ setup.py
@@ -1,13 +1,40 @@
 from distutils.core import setup
 
-with open("README.md", "r") as fh:
-  long_description = fh.read()
+long_description = '''# MailToolsBox
+
+MailToolsBox is a Python library for dealing with Email handling in easy way.
+
+## Installation
+
+Use the package manager [pip](https://ramai.io) to install foobar.
+
+```
+pip install MailToolsBox
+```
+
+## Usage
+
+```python
+import MailToolsBox.mailSender
+mail = MailToolsBox.mailSender.SendAgent(user_email='username@gmail.com', server_smtp_address='smtp.gmail.com', user_email_password='User Password', port=587)
+mail.send_mail(recipent_email='user@gmail.com', subject='This is Subject Text', message_Body='This is  Body Text')
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+'''
 
 
 setup(
   name = 'MailToolsBox',
   packages = ['MailToolsBox'],
-  version = '0.0.3',
+  version = '0.0.3.1',
   license='MIT',
   description = 'Mail Tools boxes to make developer life easier on build mail sender or even mail server',   # Give a short description about your library
   long_description=long_description,
@@ -15,7 +42,7 @@ setup(
   author = 'Rambod Ghashghai',                   # Type in your name
   author_email = 'rambod@ramai.io',      # Type in your E-Mail
   url = 'https://www.ramai.io',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/rambod/MailToolsBox/archive/0.0.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/rambod/MailToolsBox/archive/0.0.3.1.tar.gz',    # I explain this later on
   keywords = ['Mail', 'Server', 'smtp' ,  'send' , 'email', 'tools', 'box'],   # Keywords that define your package best
   install_requires=[
     # I get to this in a second
